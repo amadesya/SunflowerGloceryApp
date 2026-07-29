d9e368983013b65eef1ce27ca04e285be7137201
amadesya <ultrashitband@gmail.com>
2026-07-30 02:40:58 +0300

    feat: add app template, configure for mobile

--- modules/sunflower-auth/src/SunflowerAuthModule.ts
+++
@@ -1,5 +0,0 @@
-import { NativeModule, requireNativeModule } from 'expo';
-
-declare class SunflowerAuthModule extends NativeModule<{}> {}
-
-export default requireNativeModule<SunflowerAuthModule>('SunflowerAuth');
--- modules/sunflower-auth/src/index.ts
+++
@@ -1,4 +0,0 @@
-// Reexport the native module. On web, it will be resolved to SunflowerAuthModule.web.ts
-// and on native platforms to SunflowerAuthModule.ts
-export { default } from './SunflowerAuthModule';
-export * from './SunflowerAuth.types';
